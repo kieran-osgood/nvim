41e3c8c2a8bd97715116450df9c22fd2a86b219d
Kieran Osgood <kieranbosgood@gmail.com>
2024-02-17 09:34:02 +0000

    updating TS plugin config

---
+++ lua/plugins/tsserver.lua
@@ -0,0 +1,8 @@
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {
+    complete_function_calls = true,
+    disable_member_code_lens = true,
+  },
+}
