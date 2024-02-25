bef1adf4c1f73c050c0bcdfae77faa967bfd51aa
Kieran Osgood <kieranbosgood@gmail.com>
2024-02-25 12:11:58 +0000

    update UI to be round

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,6 +9,10 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
+  ui = {
+    border = "rounded",
+  },
+
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
@@ -33,6 +37,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
+
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
