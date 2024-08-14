f0f2fd54d0db0418583cf81e30662a48f5bb06c2
Kieran Osgood <kieran.osgood@telegraph.co.uk>
2024-08-14 13:26:55 +0100

    fix: debugprint missing cmds

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -8,43 +8,25 @@ return {
   -- not just the formal releases
   version = "*",
   opts = {
-    -- keymaps = {
-    --   normal = {
-    --     plain_below = "g?p",
-    --     plain_above = "g?P",
-    --     variable_below = "g?v",
-    --     variable_above = "g?V",
-    --     -- variable_above_alwaysprompt = nil,
-    --     -- variable_below_alwaysprompt = true,
-    --     textobj_below = "g?o",
-    --     textobj_above = "g?O",
-    --     delete_debug_prints = "g?d",
-    --     toggle_comment_debug_prints = "g?c",
-    --   },
-    --   visual = {
-    --     variable_below = "g?v",
-    --     variable_above = "g?V",
-    --   },
-    -- },
-    commands = {
-      toggle_comment_debug_prints = "ToggleCommentDebugPrints",
-      delete_debug_prints = "DeleteDebugPrints",
+    -- The 'keys' and 'cmds' sections of this configuration are only needed if
+    -- you want to take advantage of `lazy.nvim` lazy-loading.
+    keymaps = {
+      normal = {
+        plain_below = "g?p",
+        plain_above = "g?P",
+        variable_below = "g/",
+        variable_above = "g?V",
+        variable_below_alwaysprompt = nil,
+        variable_above_alwaysprompt = nil,
+        textobj_below = "g?o",
+        textobj_above = "g?O",
+        toggle_comment_debug_prints = nil,
+        delete_debug_prints = nil,
+      },
+      visual = {
+        variable_below = "g?v",
+        variable_above = "g?V",
+      },
     },
-  },
-  -- The 'keys' and 'cmds' sections of this configuration are only needed if
-  -- you want to take advantage of `lazy.nvim` lazy-loading.
-  -- keys = {
-  --   { "g?p", mode = "n" },
-  --   { "g?P", mode = "n" },
-  --   { "g?v", mode = "n" },
-  --   { "g?V", mode = "n" },
-  --   { "g?o", mode = "n" },
-  --   { "g?O", mode = "n" },
-  --   { "g?v", mode = "x" },
-  --   { "g?V", mode = "x" },
-  -- },
-  cmd = {
-    "ToggleCommentDebugPrints",
-    "DeleteDebugPrints",
   },
 }
