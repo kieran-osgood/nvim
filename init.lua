51aa93cef125765d05957967c7af84f2b76b151d
Kieran Osgood <kieran.osgood@telegraph.co.uk>
2024-08-30 12:21:41 +0100

    feat: add improved highlighting configuration

--- init.lua
+++ init.lua
@@ -9,9 +9,6 @@ vim.treesitter.language.register("markdown", "mdx")
 
 vim.o.cmdheight = 1
 
--- require("telescope").load_extension("git_worktree")
-require("telescope").load_extension("worktrees")
-
 -- Sets colors to line numbers Above, Current and Below  in this order
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
@@ -26,35 +23,32 @@ vim.o.pumheight = 100
 -- )
 
 -- This module contains a number of default definitions
-local rainbow_delimiters = require("rainbow-delimiters")
+-- local rainbow_delimiters = require("rainbow-delimiters")
 
----@type rainbow_delimiters.config
-vim.g.rainbow_delimiters = {
-  strategy = {
-    [""] = rainbow_delimiters.strategy["global"],
-    vim = rainbow_delimiters.strategy["local"],
-  },
-  query = {
-    [""] = "rainbow-delimiters",
-    lua = "rainbow-blocks",
-  },
-  priority = {
-    [""] = 110,
-    lua = 210,
-  },
-  highlight = {
-    "RainbowDelimiterRed",
-    "RainbowDelimiterYellow",
-    "RainbowDelimiterBlue",
-    "RainbowDelimiterOrange",
-    "RainbowDelimiterGreen",
-    "RainbowDelimiterViolet",
-    "RainbowDelimiterCyan",
-  },
-}
-
--- trying to conceal the quotes in json
--- vim.wo.conceallevel = vim.bo.readonly or vim.bo.buftype == "nofile" and 2 or 0
+-- -- @type rainbow_delimiters.config
+-- vim.g.rainbow_delimiters = {
+--   strategy = {
+--     [""] = rainbow_delimiters.strategy["global"],
+--     vim = rainbow_delimiters.strategy["local"],
+--   },
+--   query = {
+--     [""] = "rainbow-delimiters",
+--     lua = "rainbow-blocks",
+--   },
+--   priority = {
+--     [""] = 110,
+--     lua = 210,
+--   },
+--   highlight = {
+--     "RainbowDelimiterRed",
+--     "RainbowDelimiterYellow",
+--     "RainbowDelimiterBlue",
+--     "RainbowDelimiterOrange",
+--     "RainbowDelimiterGreen",
+--     "RainbowDelimiterViolet",
+--     "RainbowDelimiterCyan",
+--   },
+-- }
 
 require("telescope").setup({
   defaults = {
@@ -66,4 +60,39 @@ require("telescope").setup({
       git_files = { recurse_submodules = false },
     },
   },
+})
+
+require("telescope").load_extension("worktrees")
+
+require("mini.cursorword").setup({
+  delay = 20,
+})
+
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  group = vim.api.nvim_create_augroup("Color", {}),
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "LspReferenceRead", { fg = "#FF0000", underline = true })
+    vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg = "#FF0000", underline = true })
+    vim.api.nvim_set_hl(0, "LspReferenceText", { fg = "#FF0000", underline = true })
+  end,
+})
+
+-- write a vim.cmd to underline the cursor word
+-- vim.cmd("hi CursorLine cterm=underline gui=underline")
+-- vim.api.nvim_set_hl(0, "CursorLine", { underline = true })
+-- vim.cmd("hi! link MiniCursorword Visual")
+-- vim.cmd("hi! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE")
+--
+-- vim.cmd("highlight Visual cterm=NONE ctermbg=76 ctermfg=16 gui=NONE guibg=#5fd700 guifg=#000000")
+-- vim.cmd("highlight StatusLine cterm=NONE ctermbg=231 ctermfg=160 gui=NONE guibg=#ffffff guifg=#d70000")
+-- vim.cmd("highlight Normal cterm=NONE ctermbg=17 gui=NONE guibg=#00005f")
+-- vim.cmd("highlight NonText cterm=NONE ctermbg=17 gui=NONE guibg=#00005f")
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 3
+    vim.opt.conceallevel = 3
+  end,
 })
