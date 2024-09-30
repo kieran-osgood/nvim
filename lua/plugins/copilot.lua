34b0830c0e9a94a066f0100db6503c4e57eb6085
Kieran Osgood <kieran.osgood@telegraph.co.uk>
2024-09-30 10:28:01 +0100

    disable copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "https://github.com/github/copilot.vim",
-  },
-  {
-
-    "zbirenbaum/copilot-cmp",
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
+  -- {
+  --   "https://github.com/github/copilot.vim",
+  -- },
+  -- {
+  --
+  --   "zbirenbaum/copilot-cmp",
+  --   config = function()
+  --     require("copilot_cmp").setup()
+  --   end,
+  -- },
 }
