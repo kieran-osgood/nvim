85b5398fa979f84af632ba6a0cf774d317c8b5d1
Kieran Osgood <kieranbosgood@gmail.com>
2025-02-10 09:17:16 +0000

    feat: move to grug for file level search and replace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,12 +8,19 @@ vim.keymap.set("n", "<", "<<")
 -- vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Search and replace current position word
-vim.keymap.set(
-  "n",
-  "<leader>s",
-  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-  { desc = "Search and replace under cursor" }
-)
+vim.keymap.set("n", "<leader>s", function()
+  require("grug-far").open({ prefills = {
+    search = vim.fn.expand("<cword>"),
+    paths = vim.fn.expand("%"),
+  } })
+end, { desc = "Search and replace under cursor" })
+
+vim.keymap.set("v", "<leader>s", function()
+  require("grug-far").open({ prefills = {
+    search = vim.fn.expand("<cword>"),
+    paths = vim.fn.expand("%"),
+  } })
+end, { desc = "Search and replace selection" })
 
 -- Undotree
 vim.keymap.set("n", "<leader>uu", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
@@ -33,12 +40,12 @@ vim.keymap.set("n", "<leader>tW", "<cmd>Twilight<CR>", { desc = "Toggle Twilight
 
 -- Can be used to move diagnostic messages into the qflist when they're not focusable
 vim.keymap.set("n", "<leader>qf", vim.diagnostic.setqflist, { desc = "Move to QuickFix List" })
-vim.keymap.set(
-  "n",
-  "<leader><leader>",
-  "<cmd>Telescope git_files recurse_submodules=true<CR>",
-  { desc = "Find Files (git files recursive)" }
-)
+-- vim.keymap.set(
+--   "n",
+--   "<leader><leader>",
+--   "<cmd>Telescope git_files recurse_submodules=true<CR>",
+--   { desc = "Find Files (git files recursive)" }
+-- )
 
 vim.keymap.set("i", "<Tab><Tab>", 'copilot#Accept("\\<CR>")', {
   expr = true,
